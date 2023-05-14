1789cf9400957332e5a0bf8df15623f6a0864cec
natsudragneelthefiredragon <warpzephyr@gmail.com>
2023-05-14 01:05:11 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace AC4RawFormatCorrector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Raw format correcter is used to correct the rawFormat value of Armored Core 4 BND3 archives.");
+                Console.WriteLine("Yabber cannot repack this value correctly.");
+                Console.WriteLine("Drag and drop a regulation BND3 file after changes have been made onto the exe of this program.");
+                Console.WriteLine("You do not need to use this program for regulation.bin, the hash calculator will handle this.");
+                Console.WriteLine("Press any key to close this program, or click the exit button.");
+                Console.ReadKey();
+            }
+
+            if (args == null)
+                return;
+
+            foreach (string arg in args)
+            {
+                if (arg == null)
+                    continue;
+                if (!File.Exists(arg))
+                    continue;
+
+                // Read the file.
+                var file = File.ReadAllBytes(arg);
+
+                // Correct the rawFormat.
+                file[12] = 228;
+
+                // Write the file back.
+                File.WriteAllBytes(arg, file);
+            }
+        }
+    }
+}
